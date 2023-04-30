8ad3ef8d7d2ad1011a71085d7eabe47ccd0c6f52
Nguyen Hoang Kiet <skill1sp2@gmail.com>
2023-04-30 19:19:26 +0700

    Add

--- src/screens/add_spending/add_spending_page.js
+++ src/screens/add_spending/add_spending_page.js
@@ -9,6 +9,7 @@ import {
   Keyboard,
   ScrollView,
   Image,
+  Button,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons.js';
 import InputMoney from './components/input_money';
@@ -16,7 +17,24 @@ import InputSpending from './components/input_spending';
 import ItemSpending from './components/item_spending';
 import COLORS from '../../constants/colors';
 
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
 const AddSpendingPage = ({navigation}) => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = date => {
+    console.warn('A date has been picked: ', date);
+    hideDatePicker();
+  };
+
   const [more, setMore] = useState(false);
   const [money, setMoney] = useState('');
 
@@ -67,6 +85,15 @@ const AddSpendingPage = ({navigation}) => {
     // Functionality to add spending
     return (
       <>
+        <View>
+          <Button title="Show Date Picker" onPress={showDatePicker} />
+          <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="date"
+            onConfirm={handleConfirm}
+            onCancel={hideDatePicker}
+          />
+        </View>
         <View
           style={{
             paddingTop: 24,
